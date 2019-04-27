6613ec910e7bb18b3213d2e5d06b20de9f5e4019
unknown <bosu15-5645@diu.edu.bd>
2019-04-28 03:40:58 +0600

    variable using c#

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_basic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "i am a student";
+            message = message + " of DIU"; //concatenation of two string
+            Console.WriteLine(message);
+            
+            Console.ReadLine();
+              
+        }
+    }
+}
